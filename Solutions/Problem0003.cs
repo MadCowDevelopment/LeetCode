0b2ab4f81e0d18523bae60a902684ed9009ed53a
Mihi <devdumbblind@googlemail.com>
2017-10-25 14:20:14 +0800

    added solutions for problems 13 and 24

---
+++ Solutions/Problem0003.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Solutions
+{
+    public class Problem0003
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void Solution(string input, int expected)
+        {
+            Assert.Equal(expected, LengthOfLongestSubstring(input));
+        }
+
+        private static int LengthOfLongestSubstring(string s)
+        {
+            int longest = 0;
+
+            List<char> currentCharacters = new List<char>();
+
+            foreach (char c in s)
+            {
+                var indexOfC = currentCharacters.IndexOf(c);
+                if (indexOfC >= 0)
+                {
+                    currentCharacters = currentCharacters.GetRange(indexOfC + 1, currentCharacters.Count - indexOfC - 1);
+                }
+
+                currentCharacters.Add(c);
+                if (currentCharacters.Count > longest)
+                {
+                    longest = currentCharacters.Count;
+                }
+            }
+
+            return longest;
+        }
+    }
+}
---
+++ Solutions/Problem0024.cs
@@ -0,0 +1,89 @@
+﻿using Xunit;
+
+namespace Solutions
+{
+    public class Problem0024
+    {
+        [Theory]
+        [InlineData(new int[0], new int[0])]
+        [InlineData(new[] { 1 }, new[] { 1})]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 2, 1, 4, 3 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 2, 1, 4, 3, 5 })]
+        public void Solution(int[] input, int[] expected)
+        {
+            var head = ConvertArrayToListNode(input);
+
+            AssertEquality(ConvertArrayToListNode(expected), SwapPairs(head));
+        }
+
+        private static ListNode SwapPairs(ListNode head)
+        {
+            if (head == null) return null;
+            if (head.next == null) return head;
+
+            ListNode next = head.next;
+            ListNode current = head;
+            while (current?.next != null)
+            {
+                ListNode a1 = current;
+                ListNode a2 = current.next;
+                ListNode a3 = current.next.next;
+
+                ListNode a4 = current.next.next?.next;
+                a1.next = a4 ?? a3;
+                a2.next = a1;
+
+                current = a3;
+            }
+
+            return next;
+        }
+
+        private void AssertEquality(ListNode expected, ListNode actual)
+        {
+            ListNode currentExpected = expected;
+            ListNode currentActual = actual;
+            do
+            {
+                Assert.Equal(currentExpected?.val, currentActual?.val);
+                currentExpected = currentExpected?.next;
+                currentActual = currentActual?.next;
+            } while (currentExpected != null || currentActual != null);
+        }
+
+        private ListNode ConvertArrayToListNode(int[] input)
+        {
+            ListNode head = null;
+
+            if (input.Length > 0)
+            {
+                head = new ListNode(input[0]);
+                ListNode current = head;
+
+                for (int i = 1; i < input.Length; i++)
+                {
+                    current.next = new ListNode(input[i]);
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+
+            public override string ToString()
+            {
+                return val.ToString();
+            }
+        }
+    }
+}
